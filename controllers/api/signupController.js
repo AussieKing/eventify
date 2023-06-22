e3b1eea3471f99154b447358186631fc6e1fdb7a
Fergus Illman <fergus.illman.au@gmail.com>
2023-06-22 19:58:13 +0930

    added code to controllers and serverJS file

--- controllers/api/signupController.js
+++ controllers/api/signupController.js
@@ -18,7 +18,9 @@ router.post("/", async (req, res) => {
       req.session.loggedIn = true;
 
       res.status(200).json(dbUserData);
+
     });
+    
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
