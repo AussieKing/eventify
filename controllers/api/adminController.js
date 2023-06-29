26fedb29c3a987db98e11ca6ffd1f8527b7442da
Fergus Illman <fergus.illman.au@gmail.com>
2023-06-29 20:11:07 +0930

    got create event working

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -1,4 +1,21 @@
 const router = require("express").Router();
+const { Event } = require("../../models");
+
+// const NewEvent = async (req, res) => {
+//   const dbEventData = await Event.create({
+//     event_title: req.params.eventTitle,
+//     event_description: ,
+//     host_id: ,
+//     price: ,
+//     location: ,
+//     status: eventStatusEl,
+//     time: eventTimeEl,
+//   });
+
+//   const event = await Event.create(NewEvent);
+// };
+
+// const CreateEvent = await Event.create(NewEvent);
 // const isAdmin = require("../../middlewares/isAdmin");
 // If user is an admin load the admin page
 
@@ -7,26 +24,29 @@ router.get("/", (req, res) => {
 });
 
 // POST to create an event
-// router.post("/admin", async (req, res) => {
-//   try {
-//     const dbUserData = await Event.create({
-//       event_title: req.body.event_title,
-//       event_description: req.body.event_description,
-//       host_id: req.body.host_id,
-//       status: req.body.status,
-//       location: req.body.location,
-//       time: req.body.time,
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
+router.post("/", async (req, res) => {
+  try {
+    const dbEventData = await Event.create({
+      event_title: req.body.eventTitleEl,
+      event_description: req.body.eventDescriptionEl,
+      host_id: req.body.hostIDEl,
+      price: req.body.eventPriceEl,
+      location: req.body.eventLocationEl,
+      status: req.body.eventStatusEl,
+      time: req.body.eventTimeEl,
+    });
+    console.log(dbEventData);
+    res.status(200).json(dbEventData);
+  } catch (err) {
+    console.log("this error");
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // // UPDATE to update an event
-// router.post("/admin", isAdmin, async (req, res) => {});
 
 // // DELETE to delete events / users
-// router.delete("/admin/:id");
+// router.delete("/:id");
 
 module.exports = router;
--- controllers/api/signupController.js
+++ controllers/api/signupController.js
@@ -7,20 +7,20 @@ const router = require("express").Router();
 // automatically log in User after account has been created.
 
 router.post("/", async (req, res) => {
-  // try {
-  //   const dbUserData = await User.create({
-  //     username: req.body.username,
-  //     email: req.body.email,
-  //     password: req.body.password,
-  //   });
-  //   req.session.save(() => {
-  //     req.session.loggedIn = true;
-  //     res.status(200).json(dbUserData);
-  //   });
-  // } catch (err) {
-  //   console.log(err);
-  //   res.status(500).json(err);
-  // }
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get("/", async (req, res) => {
